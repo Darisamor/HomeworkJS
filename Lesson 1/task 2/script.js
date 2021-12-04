fd2ba4ebe6a26fe1af6ba2cc7cfb2b7e16748998
Darisamor <NasarHONDA@gmail.com>
2021-12-04 12:50:23 +0200

    lesson1.1

--- Lesson 1/task 2/script.js
+++
@@ -1,8 +0,0 @@
-var value = prompt("Please enter number: ")
-var num = Number(value)
-
-if (num > 0){ alert(value + " > 0")}
-    
-if (num === 0){ alert(value + " = 0")}
-
-if (num < 0){ alert(value + " < 0")}
