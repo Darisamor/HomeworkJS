7752238ec226498126428c1ba2ef366792e1e80b
Darisamor <NasarHONDA@gmail.com>
2021-12-05 19:21:53 +0200

    lesson6

--- Lesson 5/task 1/script.js
+++ Lesson 5/task 1/script.js
@@ -1,19 +1,25 @@
-// alert("Please use console!")
+alert("Please use console!")
 
 function makeBuffer() {
-    var buff = new Set();
-    return buff;
-    let print = function() {
-        let string;
-        for(var el in buff){
-            string += el;
-        }
-        return string;
-    }
+    let cache = [];
+
+    this.add = (value) => cache.push(String(value));
+    // this.add = function(value) {
+    //     cache.push(value.toString());
+    // }
+
+    this.print = () => cache.join(" ");
+
+    this.clear = () => cache = [];
+    // this.clear = () => cache.length = 0;
+
+    return this;
 }
 
 var buffer = makeBuffer();
 buffer.add("I");
 buffer.add("h8");  // :)
 buffer.add("JavaScript");
+console.log(buffer.print());
+buffer.clear();
 console.log(buffer.print());
