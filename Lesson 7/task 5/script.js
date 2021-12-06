1cb22eb6fbdec267480166f533db30545c7e9743
Darisamor <NasarHONDA@gmail.com>
2021-12-06 23:10:50 +0200

    lesson7.1

--- Lesson 7/task 5/script.js
+++ Lesson 7/task 5/script.js
@@ -1,8 +1,8 @@
 function start(){
     let sites = document.querySelectorAll('a');
     for (let i = 0; i < sites.length; i++){
-        if(sites[i].getAttribute('href') == `/http://internal.com./`) break
-        if(sites[i].getAttribute('href') == `/http://*/` || sites[i].getAttribute('href') == `/ftp://*/`){
+        if(sites[i].getAttribute('href') == `/(http://internal.com.)./`) break
+        if(sites[i].getAttribute('href') == `/(http://)./` || sites[i].getAttribute('href') == `/(ftp://)./`){
             sites[i].setAttribute('class', "external-red");
         }
     }
